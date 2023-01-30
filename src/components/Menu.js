760b8640983e8106fa63ce5d8808fcb2afb7dfd3
Joonas21 <joonas.kukonlehto@gmail.com>
2023-01-30 16:58:42 +0200

    Clened off some finnish

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom'
 const Menu = ({menu}) => {
     return (
         <div>
-            <h2>Tsekkaa lähiaikojen sääennusteet paikkakunnittain painamalla haluttua paikkakuntaa</h2>
+            <h2>Check weather for next few days according your selected location </h2>
             <Link to="/helsinki">Helsinki</Link> <Link to="/tampere">Tampere</Link> <Link to="/turku">Turku</Link>
         </div>
     )
